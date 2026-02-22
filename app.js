6b641fdfb27a2894f7f43221e6c4cf01e2a7feb2
oshadiRajapaksha29 <ohrajapaksha@gmail.com>
2026-02-22 11:32:35 +0530

    add promise

---
+++ app.js
@@ -0,0 +1,17 @@
+const condition = true;
+
+const myPromise = new Promise((resolve, reject) => {
+  if (condition) {
+    resolve('Success!');
+  } else {
+    reject('Failure!');
+  }
+});
+
+myPromise
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
